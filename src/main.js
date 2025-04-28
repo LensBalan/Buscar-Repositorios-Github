9bc53bc8b10598f3ca4143f37a20583205bfc3f8
Lens Balan <lensrambinho21@gmail.com>
2025-04-28 16:40:43 -0300

    final changes

--- src/main.js
+++ src/main.js
@@ -2,29 +2,23 @@ import api from './api';
 
 class App{
     constructor(){
-        //lista de reps
+        
         this.repositorios = [];
-
-        //pegando o formulario
         this.formulario = document.querySelector('form');
-
-        //Lista
         this.lista = document.querySelector('.list-group');
 
-        //Registrar os eventos do formulario
         this.registrar_eventos();
     }
-    registrar_eventos(){
+
+    registrar_eventos(){ //registrar os inputs no form
         this.formulario.onsubmit = evento => this.adicionar_repositorio(evento);
     }
 
     async adicionar_repositorio(evento){
         evento.preventDefault(); //evita q o form recarregue a pagina
 
-        //recuperar o valor do input
         let input = this.formulario.querySelector('input[id=repositorio]').value;
 
-        //se o input vir vazio sai da aplicação
         if(input.lenght === 0){
             return; //sempre sai da funcao
         }
@@ -35,7 +29,7 @@ class App{
         let response = await api.get(`/repos/${input}`);
         //console.log(response);
 
-        let { name, description, html_url, owner: {avatar_url} } = response.data;
+        let { name, description, html_url, owner: {avatar_url} } = response.data; //desestructuring
 
         //add o rep na lista
         this.repositorios.push({
@@ -49,8 +43,7 @@ class App{
 
     }catch(erro){
         //limpar buncando
-        this.lista.removeChild(document.querySelector('.list-group-item-warning'));
-
+        this.lista.removeChild(document.querySelector('.list-group-item-warning')); 
         //limpar erro existente
         let error = this.lista.querySelector('.list-group-item-danger');
 
@@ -64,16 +57,8 @@ class App{
         li.appendChild(txtErro);
         this.lista.appendChild(li);
     }
+}
 
-    }
-/*
-<li class="list-group-item list-group-item-action">
-        <img src="https://avatars0.githubusercontent.com/u/8083459?v=4"/>
-        <strong>Nerd Fonts</strong>
-        <p>Iconic font agregator colection and pacturing</p>
-        <a target="_blank" href="https://github.com/ryanoasis/nerd-fonts">Acessar</a>
-</li>
-*/
     apresentar_buscando(){
         let li = document.createElement('li');
         li.setAttribute('class', 'list-group-item list-group-item-warning');
@@ -82,9 +67,7 @@ class App{
         this.lista.appendChild(li);
     }
 
-
     renderizar_tela(){
-        //limpar o conteudo antigo
         this.lista.innerHTML = '';
 
         //percorrer a lista de reps e criar os elementos
@@ -122,13 +105,10 @@ class App{
             //add li como filho da ul
             this.lista.appendChild(li);
 
-            //limpar input
             this.formulario.querySelector('input[id=repositorio]').value = '';
-
             this.formulario.querySelector('input[id=repositorio]').focus();
         })
     }
-
 }
 
 new App();
